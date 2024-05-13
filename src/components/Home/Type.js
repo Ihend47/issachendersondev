e4033352b6219a4d3720552c1726c694b412d3d3
issi <issachenderson47@yahoo.com>
2024-05-13 18:46:44 -0400

    commit

---
+++ src/components/Home/Type.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Typewriter from "typewriter-effect";
+
+function Type() {
+  return (
+    <Typewriter
+      options={{
+        strings: [
+          "DevOps Engineer",
+          "Cloud Architect",
+          "Automation Specialist",
+          "Continuous Integration/Delivery Expert",
+        ],
+        autoStart: true,
+        loop: true,
+        deleteSpeed: 50,
+      }}
+    />
+  );
+}
+
+export default Type;
